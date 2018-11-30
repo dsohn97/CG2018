023fed26d3f071429ef4e60c028ca0b684ba91a3
dsohn97 <d.sohn97@gmail.com>
2018-11-30 16:04:46 +0100

    Formatierung optimiert

--- Tut08_FirstSteps/FirstSteps.cs
+++ Tut08_FirstSteps/FirstSteps.cs
@@ -18,86 +18,94 @@ namespace Fusee.Tutorial.Core
     {
         private SceneContainer _scene;
         private SceneRenderer _sceneRenderer;
-         private TransformComponent _cubeTransform;
-          private TransformComponent _cubeTransform2;
-         private ShaderEffectComponent _cubeShader;
-           private ShaderEffectComponent _cubeShader2;
-            private TransformComponent _cubeTransform3;
-           private ShaderEffectComponent _cubeShader3;
-         private float _camAngle = 0;
+        private TransformComponent _cubeTransform;
+        private TransformComponent _cubeTransform2;
+        private ShaderEffectComponent _cubeShader;
+        private ShaderEffectComponent _cubeShader2;
+        private TransformComponent _cubeTransform3;
+        private ShaderEffectComponent _cubeShader3;
+        private float _camAngle = 0;
 
-        // Init is called on startup. 
-        public override void Init()
-        {
-            // Set the clear color for the backbuffer to light green (intensities in R, G, B, A).
-            RC.ClearColor = new float4(0.1f, 0f, 0f, 1.0f);
+    // Init is called on startup. 
+    public override void Init()
+    {
+        // Set the clear color for the backbuffer to light green (intensities in R, G, B, A).
+        RC.ClearColor = new float4(0.1f, 0f, 0f, 1.0f);
             
-      // Create a scene with a cube
-      // The three components: one XForm, one Shader and the Mesh
-      _cubeTransform = new TransformComponent {Scale = new float3(1, 1, 1), Translation = new float3(0, 0, 0), Rotation = new float3(0, 0.3f, 0.3f)};
-      _cubeShader = new ShaderEffectComponent
-      { 
-          Effect = SimpleMeshes.MakeShaderEffect(new float3 (1, 1, 1), new float3 (1, 1, 1),  4)
-      };
+        // Create a scene with a cube
+        // The three components: one XForm, one Shader and the Mesh
+
+        // cube 1
+        _cubeTransform = new TransformComponent {Scale = new float3(1, 1, 1), Translation = new float3(0, 0, 0), Rotation = new float3(0, 0.3f, 0.3f)};
+        _cubeShader = new ShaderEffectComponent
+        { 
+            Effect = SimpleMeshes.MakeShaderEffect(new float3 (1, 1, 1), new float3 (1, 1, 1),  4)
+        };
        var cubeMesh = SimpleMeshes.CreateCuboid(new float3(10, 10, 10));
-    
-       _cubeTransform2 = new TransformComponent {Scale = new float3(0.5f, 0.5f, 0.5f), Translation = new float3(20, 0, 0), Rotation = new float3(0, 1.6f, 4)};
-       _cubeShader2 = new ShaderEffectComponent
-      { 
-          Effect = SimpleMeshes.MakeShaderEffect(new float3 (0.5f, 1, 1), new float3 (1, 1, 1),  4)
-      };
-      var cubeMesh2 = SimpleMeshes.CreateCuboid(new float3(10,10,5));
+
+        // cube 2
+        _cubeTransform2 = new TransformComponent {Scale = new float3(0.5f, 0.5f, 0.5f), Translation = new float3(20, 0, 0), Rotation = new float3(0, 1.6f, 4)};
+        _cubeShader2 = new ShaderEffectComponent
+        { 
+            Effect = SimpleMeshes.MakeShaderEffect(new float3 (0.5f, 1, 1), new float3 (1, 1, 1),  4)
+        };
+        var cubeMesh2 = SimpleMeshes.CreateCuboid(new float3(10,10,5));
+
+        // cube 3
         _cubeTransform3 = new TransformComponent {Scale = new float3(0.5f, 0.5f, 0.5f), Translation = new float3(-20, 0, 0), Rotation = new float3(0, 1.6f, 4)};
-       _cubeShader3 = new ShaderEffectComponent
-      { 
-          Effect = SimpleMeshes.MakeShaderEffect(new float3 (0.5f, 1, 1), new float3 (1, 1, 1),  4)
-      };
+        _cubeShader3 = new ShaderEffectComponent
+        { 
+            Effect = SimpleMeshes.MakeShaderEffect(new float3 (0.5f, 1, 1), new float3 (1, 1, 1),  4)
+        };
 
-      // Assemble the cube node containing the three components
-      var cubeNode = new SceneNodeContainer();
-      cubeNode.Components = new List<SceneComponentContainer>();
-      cubeNode.Components.Add(_cubeTransform);
-      cubeNode.Components.Add(_cubeShader);
-      cubeNode.Components.Add(cubeMesh);
+        // Assemble the cube node containing the three components
+        var cubeNode = new SceneNodeContainer();
+            cubeNode.Components = new List<SceneComponentContainer>();
+                cubeNode.Components.Add(_cubeTransform);
+                cubeNode.Components.Add(_cubeShader);
+                cubeNode.Components.Add(cubeMesh);
         var cubeNode2 = new SceneNodeContainer();
-      cubeNode2.Components = new List<SceneComponentContainer>();
-      cubeNode2.Components.Add(_cubeTransform2);
-      cubeNode2.Components.Add(_cubeShader2);
-      cubeNode2.Components.Add(cubeMesh2);
+            cubeNode2.Components = new List<SceneComponentContainer>();
+                cubeNode2.Components.Add(_cubeTransform2);
+                cubeNode2.Components.Add(_cubeShader2);
+                cubeNode2.Components.Add(cubeMesh2);
         var cubeNode3 = new SceneNodeContainer();
-      cubeNode3.Components = new List<SceneComponentContainer>();
-      cubeNode3.Components.Add(_cubeTransform3);
-      cubeNode3.Components.Add(_cubeShader3);
-      cubeNode3.Components.Add(cubeMesh2);
-      // Create the scene containing the cube as the only object
-      _scene = new SceneContainer();
-      _scene.Children = new List<SceneNodeContainer>();
-      _scene.Children.Add(cubeNode);
-      _scene.Children.Add(cubeNode2);
-      _scene.Children.Add(cubeNode3);
+            cubeNode3.Components = new List<SceneComponentContainer>();
+                cubeNode3.Components.Add(_cubeTransform3);
+                cubeNode3.Components.Add(_cubeShader3);
+                cubeNode3.Components.Add(cubeMesh2);
+                
+    // Create the scene containing the cube as the only object
+    _scene = new SceneContainer();
+        _scene.Children = new List<SceneNodeContainer>();
+            _scene.Children.Add(cubeNode);
+            _scene.Children.Add(cubeNode2);
+            _scene.Children.Add(cubeNode3);
 
-      // Create a scene renderer holding the scene above
-      _sceneRenderer = new SceneRenderer(_scene);
-        }
+    // Create a scene renderer holding the scene above
+    _sceneRenderer = new SceneRenderer(_scene);
+    }
 
         // RenderAFrame is called once a frame
         public override void RenderAFrame()
         {
-            // Clear the backbuffer
-         RC.Clear(ClearFlags.Color | ClearFlags.Depth);
+        // Clear the backbuffer
+        RC.Clear(ClearFlags.Color | ClearFlags.Depth);
 
         // Animate the camera angle
- _camAngle = _camAngle + 45.0f * M.Pi/180.0f * DeltaTime;
+        _camAngle = _camAngle + 45.0f * M.Pi/180.0f * DeltaTime;
 
- // Setup the camera 
- RC.View = float4x4.CreateTranslation(0, 0, 50) * float4x4.CreateRotationY(_camAngle);
-         // Cube
-         var i = 2;
+        // Setup the camera 
+        RC.View = float4x4.CreateTranslation(0, 0, 50) * float4x4.CreateRotationY(_camAngle);
+        
+        // Cube
+            var i = 2;
             _cubeTransform.Rotation = new float3(TimeSinceStart*i, (i/2)*TimeSinceStart, i*TimeSinceStart) ;
-             _cubeTransform2.Rotation = new float3(0, 1.6f, -i*TimeSinceStart) ;
-             _cubeTransform3.Rotation = new float3(0, 1.6f, i*TimeSinceStart) ;
+            _cubeTransform2.Rotation = new float3(0, 1.6f, -i*TimeSinceStart) ;
+            _cubeTransform3.Rotation = new float3(0, 1.6f, i*TimeSinceStart) ;
             _cubeTransform.Translation = new float3(0, 5 * M.Sin(3 * TimeSinceStart), 0);
-            _sceneRenderer.Render (RC);
+            
+        _sceneRenderer.Render (RC);
 
             // Swap buffers: Show the contents of the backbuffer (containing the currently rendered frame) on the front buffer.
             Present();
